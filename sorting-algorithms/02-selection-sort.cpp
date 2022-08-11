4e9ec0a0c37da055f4b19332d97d5d98704f13b4
Rohan Verma <rv725555@gmail.com>
2022-08-11 17:55:05 +0530

    feat: Selection sort script.

---
+++ sorting-algorithms/02-selection-sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#define SIZE 5
+
+// Sort should be in ascending order from LEFT to RIGHT
+
+// int returnMaxIndex(int arr[SIZE]) {
+//     for(int )
+// }
+
+int main() {
+    int array[SIZE] = {5, 2, 4, 3, 1};
+    int maxIndex, temp;
+
+    // Loop for making passes
+    for(int pass = 0; pass < (SIZE - 1); pass++) {
+        maxIndex = 0;
+        // Loop for finding the index position of the next largest element
+        for(int index = 1; index < ((SIZE) - pass); index++) { // We start from index 1 because by default we have assigned the maxIndex to be 0.
+            if(array[index] > array[maxIndex]) {
+                maxIndex = index;
+            };            
+        }
+        // Swap operation of the element of the next largest value with the lastmost unsorted element.
+        temp = array[maxIndex];
+        array[maxIndex] = array[SIZE - 1 - pass];
+        array[SIZE - 1 - pass] = temp;
+    }
+
+    // Loop for printing th array
+    for(int index = 0; index < SIZE; index++) {
+        std::cout << array[index] << "\n";
+    }
+
+}
