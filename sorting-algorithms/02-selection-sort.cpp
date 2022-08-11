59c313dfb447727f76cf86630bb2efa657be936e
Rohan Verma <rv725555@gmail.com>
2022-08-11 18:15:06 +0530

    chore: Beautification.

--- sorting-algorithms/02-selection-sort.cpp
+++ sorting-algorithms/02-selection-sort.cpp
@@ -3,9 +3,6 @@
 
 // Sort should be in ascending order from LEFT to RIGHT
 
-// int returnMaxIndex(int arr[SIZE]) {
-//     for(int )
-// }
 
 int main() {
     int array[SIZE] = {5, 2, 4, 3, 1};
@@ -20,7 +17,7 @@ int main() {
                 maxIndex = index;
             };            
         }
-        // Swap operation of the element of the next largest value with the lastmost unsorted element.
+        // Swap operation of the element of the next largest value with the last-most unsorted element.
         if (maxIndex != (SIZE - 1 - pass) ) { // if the position of the maxIndex is already at the end, we need not swap it.
             temp = array[maxIndex];
             array[maxIndex] = array[SIZE - 1 - pass];
