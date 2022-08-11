1bd6951e84a2173925823f62bd83869866113d9f
Rohan Verma <rv725555@gmail.com>
2022-08-11 18:40:56 +0530

    feat: Template added for sorting algorithms in CPP.

---
+++ sorting-algorithms/00-template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define SIZE 5
+
+// Sort should be in ascending order from LEFT to RIGHT
+
+int main() {
+    int array[SIZE] = {5, 2, 4, 3, 1};
+
+    // Loop for printing the array
+    for(int index = 0; index < SIZE; index++) {
+        std::cout << array[index] << "\n";
+    }
+
+}
