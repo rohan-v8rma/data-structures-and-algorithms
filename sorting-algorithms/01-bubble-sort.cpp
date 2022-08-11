8a10f4ef7a5cf8dd33f16af8dc8303ffdecf735d
Rohan Verma <rv725555@gmail.com>
2022-08-11 10:25:35 +0530

    improv: Added check for whether array is already sorted. Added project for sorting algorithms in Java.

--- sorting-algorithms/01-bubble-sort.cpp
+++ sorting-algorithms/01-bubble-sort.cpp
@@ -3,16 +3,23 @@
 
 int main() {
     int array[SIZE] = {2, 8, 6 , 4, 10, 9};
+    int temp, swap;
 
-    int temp;
     for(int pass = 0 ; pass < (SIZE - 1) ; pass++) {
+        swap = 0; // we reset the swap variable for the next pass.
         for(int comparison = 0 ; comparison < ( (SIZE - 1) - pass ) ; comparison++) {
             if(array[comparison] > array[comparison + 1]) {
                 temp = array[comparison];
                 array[comparison] = array[comparison + 1];            
-                array[comparison + 1] = temp;            
-            }
+                array[comparison + 1] = temp;
+
+                swap++;            
+            };
         }
+
+        if(swap == 0) { // If no swaps occur in a particular pass, then it is clear that the array is already sorted and we need not make any more passes.
+            break;
+        };
     }
 
     for(int index = 0; index < SIZE; index++) {
