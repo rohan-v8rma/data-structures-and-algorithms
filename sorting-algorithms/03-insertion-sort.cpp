e48ba6ea212bc6bcbdddd27ca919f8146561adf7
Rohan Verma <rv725555@gmail.com>
2022-08-11 22:22:42 +0530

    feat: Insertion sort script.

---
+++ sorting-algorithms/03-insertion-sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#define SIZE 5
+
+// Sort should be in ascending order from LEFT to RIGHT
+
+int main() {
+    int array[SIZE] = {5, 2, 4, 3, 1};
+    
+    int temp;
+
+    for(int pass = 0; pass < (SIZE - 1); pass++) { // Passes need to be 1 less than the size of the array.
+        for(int checkIndex = pass; checkIndex >= 0; checkIndex--) {
+            if(array[checkIndex + 1] < array[checkIndex]) { // If we consider the first iteration of the inner loop, array[checkIndex + 1] is the element that needs to be sorted, which is outside the portion of the array that has already been sorted.
+                temp = array[checkIndex];
+                array[checkIndex] = array[checkIndex + 1];
+                array[checkIndex + 1] = temp;       
+            }
+            else { // If the element that needs to be sorted is greater than or equal to the element that is next to it, then it means that the unsorted element has reached its correct place in the sorted array. So, we can move onto the next pass.
+                break;
+            }
+            
+        }
+    }
+
+    // Loop for printing the array
+    for(int index = 0; index < SIZE; index++) {
+        std::cout << array[index] << "\n";
+    }
+
+}
