9406868610b57c016e150d34569093900990a259
Rohan Verma <rv725555@gmail.com>
2022-08-11 18:03:41 +0530

    feat: Template added for sorting algorithms in Java.

--- sorting-algorithms/SortingAlgorithms/src/Template.java
+++ sorting-algorithms/SortingAlgorithms/src/Template.java
@@ -1,5 +1,16 @@
-public class Main {
+import java.util.Arrays;
+
+public class Template {
+    static int size = 5;
+
     public static void main(String[] args) {
-//        System.out.println("This is the main class.");
+        int[] array = {2, 1, 5, 4, 3};
+//        int[] array = {1, 2, 3, 4, 5}; Check whether already sorted array is detected and loop is broken
+        sortName(array);
+        System.out.printf(Arrays.toString(array));
+    }
+
+    static void sortName(int[] array) {
+        // Code for sorting goes here and change the function name.
     }
 }
