72eb2415cb5a128c868f97410dc58b796da4dca5
Rohan Verma <rv725555@gmail.com>
2022-08-25 11:19:29 +0530

    fix: Rectified logic for finding the middle element in in-place merge sort.

--- sorting-algorithms/SortingAlgorithms/src/MergeSortInPlace.java
+++ sorting-algorithms/SortingAlgorithms/src/MergeSortInPlace.java
@@ -10,17 +10,21 @@ public class MergeSortInPlace {
 
         int subArrLen = end - start;
 
-        if(subArrLen == 1) { // Base condition of recursive function, where no elements are in the unsorted array
+        if(subArrLen == 1) { // Base condition of recursive function, since when the sub-array has 1 element, we can't divide it any further.
             return;
         }
-
-        int middle = subArrLen / 2;
+//        int middle = subArrLen / 2;
+        /*
+        We replaced the ABOVE assignment with the below one because the above assignment is only OK in the case where the sub-array is from 0 to end.
+        Suppose the array is from 3 to 5, the middle index comes out to be (5 - 3) / 2, which is 1. Obviously, 1 doesn't come in between 3 and 5.
+        The BELOW assignment gives us the average of 3 and 5, which is 4. This is the actual middle element of the sub-array.
+        */
+        int middle = (start + end) / 2;
 
         mergeSortInPlace(unsortedArr, start, middle);
         mergeSortInPlace(unsortedArr, middle, end);
 
         mergeInPlace(unsortedArr, start, middle, end);
-        
     }
 
     static void mergeInPlace(int[] arr, int start, int middle, int end) {
@@ -38,15 +42,16 @@ public class MergeSortInPlace {
                 mergedArray[mergePtr++] = arr[rightPtr++];
             }
         }
+
         while(leftPtr < middle) {
             mergedArray[mergePtr++] = arr[leftPtr++];
         }
+
         while (rightPtr < end) {
             mergedArray[mergePtr++] = arr[rightPtr++];
         }
 
         mergePtr = 0;
-
 
         // CHECKED
         // For changing the values in the sub-array, using the values of the mixed array.
