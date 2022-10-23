732013cb8ca5f69fe1457f47e9b53fad30d6d9bd
Rohan Verma <rv725555@gmail.com>
2022-10-24 00:14:05 +0530

    improv: Abstracted user-defined Array class to header file.

--- data-structures/08-D-binary-tree-to-BST.cpp
+++ data-structures/08-D-binary-tree-to-BST.cpp
@@ -1,68 +1,8 @@
 #include <iostream>
+#include "00-Array.h" // User-defined header-file containing Array class
 using namespace::std;
 
 // Converting binary tree to BST.
-
-class Array {
-private:
-    int capacity = 10;
-    int size = 0;
-    int* arrayPointer;
-public:
-    Array() {
-        arrayPointer = new int[capacity];
-    }
-
-    Array(int capacity) {
-        arrayPointer = new int[capacity];
-    }
-
-    void ensureCapacity() {
-        if(size == capacity) {
-            int* tempPointer = new int[2 * capacity];
-
-            for(int index = 0; index < capacity; index++) {
-                tempPointer[index] = arrayPointer[index];
-            }
-            
-            arrayPointer = tempPointer;
-            capacity *= 2;
-        }
-    }
-
-    void insertElement(int element) {
-        ensureCapacity();
-
-        // Suppose array has no elements, then size is 0. So, we insert an element at 0th index position, then increment the size variable.
-        arrayPointer[size++] = element;
-    }
-
-    int deleteElementFromFront() {
-        int returnVal = arrayPointer[0];
-
-        for(int index = 1; index < size; index++) {
-            arrayPointer[index - 1] = arrayPointer[index];
-        }
-        
-        size--;
-        
-        return returnVal;
-    }
-
-    void sortArray() {
-        for(int index = 1; index < size; index++) {
-            int tempIndex = index;
-            int tempVar;
-            while( (tempIndex >= 1) && (arrayPointer[tempIndex - 1] > arrayPointer[tempIndex]) ) {
-                tempVar = arrayPointer[tempIndex - 1];
-                arrayPointer[tempIndex - 1] = arrayPointer[tempIndex];
-                arrayPointer[tempIndex] = tempVar;
-                tempIndex--;
-            }
-        }
-        // Array is sorted
-    }
-};
 
 class Node {
     friend void inOrderTraversal(Node* rootNode);
