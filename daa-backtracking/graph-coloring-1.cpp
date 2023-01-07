8b67d3f6ddd087635f992fee954764e3eae9369a
Rohan Verma <rv725555@gmail.com>
2023-01-07 18:41:50 +0530

    chore: Made some changes to variable naming.

--- daa-backtracking/graph-coloring-1.cpp
+++ daa-backtracking/graph-coloring-1.cpp
@@ -7,16 +7,16 @@ const int N = 5;
 // Let the color numbers start from 0. Uncolored vertices have color as -1.
 // In this way, the color numbers range from 0 to `N - 1` (4, in this case).
 
-void colorVertex(int vertexNumber, int colorNumber[N], int adjacencyMatrix[N][N], int possibleColors[N][N]) {
+void colorVertex(int vertexNumber, int colorArray[N], int adjacencyMatrix[N][N], int possibleColors[N][N]) {
     
     // `toVertex` represents the vertices in the graph which can have 
     for(int toVertex = 0; toVertex < N; toVertex++) {
-        if ( (adjacencyMatrix[vertexNumber][toVertex] == 1) && (colorNumber[toVertex] != -1) ) {
+        if ( (adjacencyMatrix[vertexNumber][toVertex] == 1) && (colorArray[toVertex] != -1) ) {
             // ( (There is an edge from `vertexNumber` to `toVertex`) && (The `toVertex` is COLORED) )
             
             // NOTE: The case where `vertexNumber` == `toVertex` is avoided over here because `toVertex` represents only COLOR vertices, but `vertexNumber` isn't colored yet.
 
-            possibleColors[vertexNumber][colorNumber[toVertex]] = 0; 
+            possibleColors[vertexNumber][colorArray[toVertex]] = 0; 
             // Removing the color of the `toVertex` from the list of possible colors for `vertexNumber`
         }
     }
@@ -25,7 +25,7 @@ void colorVertex(int vertexNumber, int colorNumber[N], int adjacencyMatrix[N][N]
     while(possibleColors[vertexNumber][colorToBeUsed] == 0) {
         colorToBeUsed++;
     }
-    colorNumber[vertexNumber] = colorToBeUsed;
+    colorArray[vertexNumber] = colorToBeUsed;
 }
 
 void graphColoring(int adjacencyMatrix[N][N]) {
@@ -42,12 +42,12 @@ void graphColoring(int adjacencyMatrix[N][N]) {
         {1, 1, 1, 1, 1}
     };
 
-    int colorNumber[N] = {-1, -1, -1, -1, -1};
+    int colorArray[N] = {-1, -1, -1, -1, -1};
 
     printf("Vertex | Color\n");
     for(int vertex = 0; vertex < N; vertex++) {
-        colorVertex(vertex, colorNumber, adjacencyMatrix, possibleColors);
-        printf("%4d   | %3d\n", vertex, colorNumber[vertex]);    
+        colorVertex(vertex, colorArray, adjacencyMatrix, possibleColors);
+        printf("%4d   | %3d\n", vertex, colorArray[vertex]);    
     }
 
     
--- daa-backtracking/graph-coloring-2.cpp
+++ daa-backtracking/graph-coloring-2.cpp
@@ -9,11 +9,11 @@ const int N = 5;
 
 //! Imagine this as a tree structure where; before following a particular branch of the tree in our attempt to find a solution, we first check whether the branch we are about to select has a dead end. Dead end means the color we are trying to use would violate the constraints of the graph coloring problem.
 
-int colorVertex(int vertexNumber, int colorToBeUsed, int adjacencyMatrix[N][N], int colorNumber[N]) {
+int colorVertex(int vertexNumber, int colorToBeUsed, int adjacencyMatrix[N][N], int colorArray[N]) {
     
     // `toVertex` represents the vertices in the graph which can have 
     for(int toVertex = 0; toVertex < N; toVertex++) {
-        if ( (adjacencyMatrix[vertexNumber][toVertex] == 1) && (colorNumber[toVertex] == colorToBeUsed) ) {
+        if ( (adjacencyMatrix[vertexNumber][toVertex] == 1) && (colorArray[toVertex] == colorToBeUsed) ) {
             // ( (There is an edge from `vertexNumber` to `toVertex`) && 
             // (The `toVertex` is COLORED using the same color as the one we wish to use, which is NOT viable in graph coloring problem) )
 
@@ -23,13 +23,13 @@ int colorVertex(int vertexNumber, int colorToBeUsed, int adjacencyMatrix[N][N], 
     }
 
     // If control of the program reaches here, it means no adjacent vertices of `vertexNumber` have the same color as the one we wish to use.
-    colorNumber[vertexNumber] = colorToBeUsed;
+    colorArray[vertexNumber] = colorToBeUsed;
     
     // Now, trying to color the next vertex, provided it exists.
     if(vertexNumber + 1 <= N) {
             for(int potentialColor = 0; potentialColor < N; potentialColor++) {
             
-            if(colorVertex(vertexNumber + 1, potentialColor, adjacencyMatrix, colorNumber)) {
+            if(colorVertex(vertexNumber + 1, potentialColor, adjacencyMatrix, colorArray)) {
                 // If the `potentialColor` was used to color the vertex, i.e. 1 was returned, we break the loop of trying more colors.
                 break;
             }
@@ -45,23 +45,18 @@ int colorVertex(int vertexNumber, int colorToBeUsed, int adjacencyMatrix[N][N], 
 }
 
 void graphColoring(int adjacencyMatrix[N][N]) {
-
-
-    // We initialize the values at all indices with 1, because the indices which are (0 through 4); represent the potential colors. 1 means that the color can be used to color the vertex; 0 means that it can't.
-    
-    // Worst case scenario is that all vertices are different colors, which is why we have 5 possible colors.
    
-    int colorNumber[N] = {-1, -1, -1, -1, -1};
+    int colorArray[N] = {-1, -1, -1, -1, -1}; // -1 means the vertices (represented by the index positions) aren't colored yet.
 
     // Since no vertices are colored yet, we can choose to color the 0th vertex with the `0` color. Or any other color
-    colorVertex(0, 0, adjacencyMatrix, colorNumber);
+    colorVertex(0, 0, adjacencyMatrix, colorArray);
     // Now, the coloring of other vertices will happen internally using recursion and backtracking.
 
 
     printf("Vertex | Color\n");
     for(int vertex = 0; vertex < N; vertex++) {
         // Printing the vertices as letters.
-        printf("%4c   | %3d\n", vertex + 65, colorNumber[vertex]);    
+        printf("%4c   | %3d\n", vertex + 65, colorArray[vertex]);    
     }
 }
 
