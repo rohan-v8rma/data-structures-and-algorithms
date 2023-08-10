e982aa21f4fbdf91875d2111ea61a299eb635274
Rohan Verma <rv725555@gmail.com>
2023-08-10 10:43:54 +0530

    improv: Made n.log(n) solution work.

--- solved-problems/arrays/using-maps/two-sum-LEET.java
+++ solved-problems/arrays/using-maps/two-sum-LEET.java
@@ -1,5 +1,6 @@
 // Problem link: https://leetcode.com/problems/two-sum
 import java.util.*;
+import java.util.*;
 
 class Solution {
     /* 
@@ -34,7 +35,14 @@ class Solution {
         if(end <= start) {
             return;
         }
+
         int pivot = (start + end) / 2;
+
+        for(int index = start; index <= end; index++) {
+            System.out.printf("%d, ", arr[index]);
+        }
+        System.out.printf("\npivot: %d\n", arr[pivot]);
+
 
         swap(arr, end, pivot);
         swap(arr2, end, pivot);
@@ -42,20 +50,40 @@ class Solution {
         int left = start;
         int right = end - 1;
 
-        while(left < right) {
-            while(left < (end - 1) && arr[left] < arr[end]) {
+        while(left <= right) {
+            while(
+                left < end
+                && 
+                /* 
+                The element pointed to by left pointer 
+                has to be less than pivot
+                */
+                arr[left] <= arr[end]
+            ) {
                 left++;
             }
-
-            while(right > 0 && arr[right] >= arr[end]) {
+            
+            while(
+                right > start 
+                && 
+                /* 
+                The element pointed to by right pointer 
+                has to be less than or equal to pivot
+                */
+                arr[right] > arr[end]
+                ) {
                 right--;
             }
 
+            // The elements pointed to by left and right are swappable.
             if(left < right) {
                 swap(arr, left, right);
                 swap(arr2, left, right);
                 left++;
                 right--;
+            }
+            else {
+                break;
             }
         }
 
@@ -64,13 +92,21 @@ class Solution {
 
         quickSort(arr, arr2, start, left - 1);
         quickSort(arr, arr2, left + 1, end);
+
+        for(int index = start; index <= end; index++) {
+            System.out.printf("%d, ", arr[index]);
+        }
+        System.out.println();
     }
 
     /* 
-    O(n.log(n)) solution. Sort the array then use 2 pointers
-    Failed: Does not work for negative targets
+    O(n.log(n)) solution, but operations extremely costly
+    
+    This is because secondary array has to be maintained and sorted USING 
+    custom quicksort function, so that we are able to preserve the
+    index values.
     */
-    public int[] twoSum2(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         int[] solution = new int[2];
 
         int n = nums.length;
@@ -81,38 +117,32 @@ class Solution {
 
         quickSort(nums, arr2, 0, n - 1);
 
+        for(int index = 0; index < n; index++) {
+            System.out.printf("%d, ", nums[index]);
+        }
+
+        // This pointer points to the smallest element in the array
         int pointer1 = 0;
-        int pointer2 = 1;
-        while(
-            ((nums[pointer1] + nums[pointer2]) < target)
-            &&
-            (pointer2 < (n - 1))
-        ) {
-            pointer2++;
+        // This pointer points to the largest element in the array
+        int pointer2 = nums.length - 1;
+
+        while(pointer1 < pointer2) {
+            
+            // Making pointer1 point to a larger element
+            if(nums[pointer1] + nums[pointer2] < target) {
+                pointer1++;
+            }
+            // Making pointer2 point to a smaller element
+            else if(nums[pointer1] + nums[pointer2] > target) {
+                pointer2--;
+            }
+            else {
+                break;
+            }
         }
 
-        if((nums[pointer1] + nums[pointer2]) > target) {
-            pointer2--;
-        }
-
-        while(
-            ((nums[pointer1] + nums[pointer2]) < target)
-            &&
-            (pointer1 < (n - 1))
-            &&
-            (pointer1 + 1 != pointer2)
-        ) {
-            pointer1++;
-        }
-
-        if((nums[pointer1] + nums[pointer2]) > target) {
-            pointer1--;
-        }
-
-        if((nums[pointer1] + nums[pointer2]) == target) {
-            solution[0] = arr2[pointer1];
-            solution[1] = arr2[pointer2];
-        }
+        solution[0] = arr2[pointer1];
+        solution[1] = arr2[pointer2];        
 
         return solution;
     }
@@ -124,7 +154,7 @@ class Solution {
     
     This is better since we can buy more memory but time cannot be bought.
     */
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum3(int[] nums, int target) {
         
         Map<Integer, Integer> numbersSeen = new HashMap<>();
 
