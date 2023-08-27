25c2cf1546d075a8c9aad380b69bd555540c2881
Rohan Verma <rv725555@gmail.com>
2023-08-27 21:58:47 +0530

    improv: Changed DFS implementation of top sort.

--- solved-problems/graphs/topological-sort-DFS-1-GFG.java
+++ solved-problems/graphs/topological-sort-DFS-1-GFG.java
@@ -1,56 +1,44 @@
 // https://practice.geeksforgeeks.org/problems/topological-sort/0
 
-class Solution
-{
-    static void dfs(int src, AnswerObj ans, boolean[] visited, ArrayList<ArrayList<Integer>> adj) {
-        visited[src] = true;
+class Solution {
+    
+    // DFS approach for top sort.
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+        visited = new boolean[V];
+        topSortIdx = V - 1;
+        topSortArr = new int[V];
         
-        for(int adjacentNode: adj.get(src)) {
-            if(!visited[adjacentNode]) {
-                dfs(adjacentNode, ans, visited, adj);
+        for(int startV = 0; startV < V; startV++) {
+            if(!visited[startV]) {
+                dfs(startV, adj);
+            }
+        }
+        
+        return topSortArr;
+    }
+    
+    static boolean[] visited;
+    static int topSortIdx;
+    static int[] topSortArr;
+    
+    static void dfs(int node, ArrayList<ArrayList<Integer>> adj) {
+        // Marking the node as visited.
+        visited[node] = true;
+        
+        for(int adjNode: adj.get(node)) {
+            if(!visited[adjNode]) {
+                dfs(adjNode, adj);
             }
         }
         
         /* 
-        Before backtracking, we insert the current vertex. 
-        
+        After all adjacent nodes have been visited, we add the current vertex 
+        to the top sort order AHEAD of all added nodes so that property of top 
+        sort is maintained.
+
         This way in the base case, when the vertex has no adjacent vertices, 
         it is inserted in the end of the topological order.
-        
-        This is in accordance with the principles of topological sort.
         */
-        ans.insert(src);
-        return;
+        topSortArr[topSortIdx--] = node;
     }
-    
-    //Function to return list containing vertices in Topological order. 
-    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
-    {
-        AnswerObj answer = new AnswerObj(V);
-        
-        boolean[] visited = new boolean[V];   
-        
-        for(int src = 0; src < V; src++) {
-            if(!visited[src]) {
-                dfs(src, answer, visited, adj);
-            }
-        }
-        
-        // add your code here
-        return answer.topSortedVertices;
-    }
-}
-
-class AnswerObj {
-    int[] topSortedVertices;
-    int currentIndex;
-    
-    AnswerObj(int V) {
-        topSortedVertices = new int[V];
-        currentIndex = V - 1;
-    }
-    void insert(int vertex) {
-        topSortedVertices[currentIndex--] = vertex;
-        return;
-    } 
 }
