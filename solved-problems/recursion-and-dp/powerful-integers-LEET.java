233c525ad9b3787ce35d68118a15cbb95c4dcd82
Rohan Verma <rv725555@gmail.com>
2024-01-07 22:12:07 +0530

    feat: Powerful integers, LEETCODE 970.

---
+++ solved-problems/recursion-and-dp/powerful-integers-LEET.java
@@ -0,0 +1,82 @@
+// https://leetcode.com/problems/powerful-integers
+
+class Solution {
+    // static boolean[][] alreadyChecked;
+
+    static int getMaxPow(int n, int bound) {
+        // Log can't be calculated in these cases.
+        if(n == 1 || bound == 0) {
+            return 0;
+        }
+
+        // The power of `n` which will get it closest to bound without exceeding it.
+        return (int)(Math.log(bound) / Math.log(n));
+    }
+
+    // // Recursive method
+    // static void checkIfPowerful(
+    //     int x, 
+    //     int y, 
+    //     int bound, 
+    //     Set<Integer> powerfulList, 
+    //     int xPow, 
+    //     int yPow
+    // ) {
+    //     if(alreadyChecked[xPow][yPow]) {
+    //         return;
+    //     }
+
+    //     int result = (int)(Math.pow(x, xPow) + Math.pow(y, yPow));
+
+    //     if(result <= bound) { 
+    //         powerfulList.add(result);
+    //         alreadyChecked[xPow][yPow] = true;
+
+    //         if(x != 1 && xPow + 1 < alreadyChecked.length) {
+    //             checkIfPowerful(x, y, bound, powerfulList, xPow + 1, yPow);
+    //         }
+            
+    //         if(y != 1 && yPow + 1 < alreadyChecked[0].length) {
+    //             checkIfPowerful(x, y, bound, powerfulList, xPow, yPow + 1);
+    //         }
+    //     }
+    // }
+
+    // SELF DEVELOPED ALGO
+    // public List<Integer> powerfulIntegers(int x, int y, int bound) {
+    //     Set<Integer> powerfulList = new HashSet<>();
+
+    //     // Max power of X, which won't exceed bound.
+    //     int maxPowOfX = getMaxPow(x, bound);
+    //     // Max power of Y, which won't exceed bound.
+    //     int maxPowOfY = getMaxPow(y, bound);
+
+    //     // This is the DP matrix for preventing repeated recursion calls.
+    //     alreadyChecked = new boolean[maxPowOfX + 1][maxPowOfY + 1];
+
+    //     checkIfPowerful(x, y, bound, powerfulList, 0, 0);
+
+    //     return new ArrayList<>(powerfulList);
+    // }
+
+    // OPTIMAL
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        Set<Integer> powerfulList = new HashSet<>();
+
+        // Max power of X, which won't exceed bound.
+        int maxPowOfX = getMaxPow(x, bound);
+        // Max power of Y, which won't exceed bound.
+        int maxPowOfY = getMaxPow(y, bound);
+
+        for(int powX = 0; powX <= maxPowOfX; powX++) {
+            for(int powY = 0; powY <= maxPowOfY; powY++) {
+                int result = (int)(Math.pow(x, powX) + Math.pow(y, powY));
+                if(result <= bound) {
+                    powerfulList.add(result);
+                }
+            }    
+        }
+        
+        return powerfulList.stream().collect(Collectors.toList());
+    }
+}
