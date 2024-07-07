33ab9cda22fc9ec07d389d93e8d2366f431b5835
Rohan Verma <rv725555@gmail.com>
2024-07-07 17:08:05 +0530

    feat: Simplified DFS calls.

--- solved-problems/graphs/ShortestPathUndirectedUnweightedGraphNINJA.java
+++ solved-problems/graphs/ShortestPathUndirectedUnweightedGraphNINJA.java
@@ -1,8 +1,11 @@
 // https://www.codingninjas.com/studio/problems/shortest-path-in-an-unweighted-graph_981297
 
-public class Solution {
+import java.util.*;
 
-	// TLE: Modified version of DFS, that has complexity greater than O(V + E)
+class Solution {
+	/*
+	____TLE: Modified version of DFS, that has complexity greater than O(V + E)____
+	*/
 	// public static LinkedList<Integer> shortestPath(int[][] edges, int n, int m, int s, int t) {
 	// 	// Write your code here.
 	// 	adjList = new ArrayList<>();
@@ -22,7 +25,6 @@ public class Solution {
 		
 	// 	LinkedList<Integer> currentPath = new LinkedList<>();
 
-	// 	visited[s] = true;
 	// 	dfs(s, t, currentPath);
 
 	// 	return path;
@@ -33,6 +35,7 @@ public class Solution {
 	// static boolean[] visited;
 	
 	// static void dfs(int currentNode, int destination, LinkedList<Integer> currentPath) {
+	// 	visited[currentNode] = true;
 	// 	currentPath.add(currentNode);
 
 	// 	if(currentNode == destination) {
@@ -47,20 +50,97 @@ public class Solution {
 	// 	}
 	// 	else if(path != null && currentPath.size() >= path.size()) {}
 	// 	else {
-			
 	// 		for(int adjNode: adjList.get(currentNode)) {
 	// 			if(!visited[adjNode]) {
-	// 				visited[adjNode] = true;
 	// 				dfs(adjNode, destination, currentPath);
-	// 				visited[adjNode] = false;
+					
 	// 			}
 	// 		}
 	// 	}
 
 	// 	currentPath.removeLast();
+	// 	visited[currentNode] = false;
 	// }
 
-	// BFS appproach
+	/*
+	____TLE: Modified version of DFS, that has complexity greater than O(V + E)____
+
+	Changing to parent array doesn't really help because problem is in O(V + E) part
+	*/
+	// public static LinkedList<Integer> shortestPath(int[][] edges, int n, int m, int s, int t) {
+	// 	// Write your code here.
+	// 	adjList = new ArrayList<>();
+
+	// 	for(int house = 0; house <= n; house++) {
+	// 		adjList.add(new ArrayList<>());
+	// 	}
+
+	// 	for(int[] edge: edges) {
+	// 		adjList.get(edge[0]).add(edge[1]);
+	// 		adjList.get(edge[1]).add(edge[0]);
+	// 	}
+
+	// 	visited = new boolean[n + 1];
+
+	// 	path = null;
+		
+	// 	parent = new int[n + 1];
+
+	// 	pathLen = Integer.MAX_VALUE;
+
+	// 	parent[s] = -1;
+
+	// 	dfs(s, t, -1);
+
+	// 	return path;
+	// }
+	
+	// static int[] parent;
+	// static LinkedList<Integer> path = null;
+	// static int pathLen;
+	// static ArrayList<ArrayList<Integer>> adjList;
+	// static boolean[] visited;
+
+	// static void getPath(int destination) {
+	// 	LinkedList<Integer> newPath = new LinkedList<>();
+
+	// 	while(destination != -1) {
+	// 		newPath.addFirst(destination);
+	// 		destination = parent[destination];
+	// 	}
+		
+	// 	path = newPath;
+	// }
+	
+	// static void dfs(
+	// 	int currentNode, 
+	// 	int destination, 
+	// 	int currPathLen
+	// ) {
+	// 	currPathLen++;
+	// 	visited[currentNode] = true;
+
+	// 	if(currentNode == destination && currPathLen < pathLen) {
+	// 		pathLen = currPathLen;
+	// 		getPath(destination);
+	// 	}
+	// 	else {	
+	// 		for(int adjNode: adjList.get(currentNode)) {
+	// 			if(!visited[adjNode]) {
+	// 				int oldParent = parent[adjNode];
+	// 				parent[adjNode] = currentNode;
+	// 				dfs(adjNode, destination, currPathLen);
+	// 				parent[adjNode] = oldParent;
+	// 			}
+	// 		}
+	// 	}
+
+	// 	visited[currentNode] = false;
+	// }
+
+	/*
+	____BFS appproach____
+	*/
 	public static LinkedList<Integer> shortestPath(int[][] edges, int n, int m, int s, int t) {
 		
 		ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
